73c2864f5cb5d1fdbf135249ccb0805b43961aca
Roberto Mantas Nakhai <r.mantas.nakhai@gmail.com>
2020-06-23 12:48:30 +0200

    change matching text for files

--- modis_operation.py
+++ modis_operation.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+
 def createFolder(directory):
     try:
         if not os.path.exists(directory):
@@ -10,17 +11,15 @@ def createFolder(directory):
 def modis_operation(list_folder, source):
     path = os.path.dirname(os.path.realpath(__file__))
     for files in source:
-        if files.startswith("A") or files == ".DS_Store" or files.endswith == "txt" or files.endswith(".py"):
-            continue
         folder_name = (files[8:16]+"_")
-        if files.startswith("MOD11A1") and folder_name not in list_folder:
+        if files.endswith(".hdf") and folder_name not in list_folder:
             createFolder('./' + str(folder_name) + '/')
             list_folder.append(folder_name)
 
 
     os.system("modis_mosaic.py -o /Mosaic -v ./files.txt")
     for name in list_folder:
-        os.system("modis_convert.py -v -s \"( 1 )\" -o ./Layer1/Mosaic1_"+str(name)+" -e 4326 "+ path + "/"+str(name)+"/Mosaic_LST_Day_1km.vrt")
-        os.system("modis_convert.py -v -s \"( 1 )\" -o ./Layer5/Mosaic5_"+str(name)+" -e 4326 "+ path + "/"+str(name)+"/Mosaic_LST_Night_1km.vrt")
+        os.system("modis_convert.py -v -s \"( 1 )\" -o ./Layer1/Mosaic1_"+str(name)+" -e 4326 "+ path + "/"+str(name)+"/Mosaic_LST_Day_1km.vrt") #Mosaic for Layer 1
+        os.system("modis_convert.py -v -s \"( 1 )\" -o ./Layer5/Mosaic5_"+str(name)+" -e 4326 "+ path + "/"+str(name)+"/Mosaic_LST_Night_1km.vrt") #Mosaic for Layer 5
         shutil.rmtree(path + "/"+str(name)+"/")
 modis_operation([], os.listdir("./"))
