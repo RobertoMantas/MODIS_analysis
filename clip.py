a5ff2031c45bad6cfab11e719065013602da72b8
Roberto Mantas Nakhai <r.mantas.nakhai@gmail.com>
2020-07-08 16:08:38 +0200

    Update clip.py

--- clip.py
+++ clip.py
@@ -1,6 +1,7 @@
 import os
 
-def modis_operation(list_folder, source, path):
+#Cliping functions. Cut the boundaries outside the given shapefile. The shape file should be named "alla_lan".
+def clip_operation(list_folder, source, path):
     print(path)
     if path.endswith("Layer1/"):
         clipName = "clip1_"
@@ -16,7 +17,8 @@ def modis_operation(list_folder, source, path):
 
     for name in list_folder:
         os.system("gdalwarp -co COMPRESS=DEFLATE -dstalpha -cutline alla_lan.shp -crop_to_cutline " + path + mosaicName + name + "_.tif "+ path + clipName + name+".tif")
+    #In case you want to remove the mosaic files after clipping, uncomment the following line.
     #os.system("rm " + path + "/Mosaic*")
 
-modis_operation([], os.listdir("./Layer1"), os.path.dirname(os.path.realpath(__file__))+"/Layer1/")
-modis_operation([], os.listdir("./Layer5"), os.path.dirname(os.path.realpath(__file__))+"/Layer5/")
+clip_operation([], os.listdir("./Layer1"), os.path.dirname(os.path.realpath(__file__))+"/Layer1/")
+clip_operation([], os.listdir("./Layer5"), os.path.dirname(os.path.realpath(__file__))+"/Layer5/")
